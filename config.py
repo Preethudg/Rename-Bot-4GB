48afd27d08ed913a0e1d897454a41f7de9b4e7bc
Preethudg <sanjanasanjanadg59@gmail.com>
2025-03-17 08:57:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ STRING_SESSION = os.environ.get("STRING_SESSION", "")
 
 
 # Log & Force Channel Config
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "kannadamovieskannadadubb")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
 
 
