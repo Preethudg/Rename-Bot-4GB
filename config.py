0bfb23fa584cf0cfedc4fa78cb3d0d5a8fe7ba9e
Preethudg <sanjanasanjanadg59@gmail.com>
2025-03-17 08:57:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ STRING_SESSION = os.environ.get("STRING_SESSION", "")
 
 # Log & Force Channel Config
 FORCE_SUBS = os.environ.get("FORCE_SUBS", "kannadamovieskannadadubb")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002334874259"))
 
 
 # Mongo DB Database Config
